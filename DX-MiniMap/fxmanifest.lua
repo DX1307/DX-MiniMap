729bb0a4ef701b592f81be9c9f99dc5a287a0902
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-08-19 22:07:27 +0700

    Add files via upload

---
+++ DX-MiniMap/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "DX-MiniMap"
+author  "DX-HIGH Development"
+repository "https://github.com/DX1307"
+description "MiniMap Script"
+
+this_is_a_map 'yes'
+
+client_script 'client.lua'
